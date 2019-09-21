d1ccc958c2f50c6f488ff270332926c5d5ac51b9
mikearaya <mikaelaraya12@gmail.com>
2019-09-21 20:16:41 +0300

    added end to end test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -88,5 +88,24 @@ describe('Lottery Contract', () => {
     }
   });
 
-  
+  it('sends money to the winner and resets the player array', async() => {
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    });
+
+  const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+  await lottery.methods.pickWinner().send({
+    from : accounts[0]
+  });
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+  } )
+
 })
